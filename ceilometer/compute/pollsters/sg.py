4fb6fcfeaf1fca172edd260cfbd97dcf4a05c3ce
root <lulk@cn.ibm.com>
2016-02-03 17:04:03 +0800

    security group metrics

---
+++ ceilometer/compute/pollsters/sg.py
@@ -0,0 +1,155 @@
+# Copyright 2012 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import copy
+
+from oslo_log import log
+
+import ceilometer
+from ceilometer.compute import pollsters
+from ceilometer.compute.pollsters import util
+from ceilometer.i18n import _, _LW
+from ceilometer import sample
+from ceilometer.compute import sg_meter
+from oslo_utils import timeutils
+from ceilometer.agent import plugin_base
+
+LOG = log.getLogger(__name__)
+class _Base(plugin_base.PollsterBase):
+
+    @property
+    def default_discovery(self):
+        return 'local_ports'
+
+    @staticmethod
+    def make_sg_sample(port, name, type, unit, volume):
+        import pdb;pdb.set_trace();
+        samp = sample.Sample(
+            name=name,
+            type=type,
+            unit=unit,
+            volume=volume,
+            user_id=port['network_id'],
+            project_id=port['tenant_id'],
+            resource_id='.'.join([port['tenant_id'], port['tenant_id'], port['device_id'],type]),
+            timestamp=timeutils.utcnow().isoformat(),
+            resource_metadata=None,
+        )
+        #print '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
+        #print samp
+        #print '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
+        return samp
+
+    def _record_poll_time(self):
+        """Method records current time as the poll time.
+
+        :return: time in seconds since the last poll time was recorded
+        """
+        current_time = timeutils.utcnow()
+        duration = None
+        if hasattr(self, '_last_poll_time'):
+            duration = timeutils.delta_seconds(self._last_poll_time,
+                                               current_time)
+        self._last_poll_time = current_time
+        return duration
+
+    def _get_sg_for_port(self, port):
+        return sg_meter.SGmetering(port)
+
+    def get_samples(self, manager, cache, resources):
+        self._inspection_duration = self._record_poll_time()
+        for port in resources:
+            LOG.debug('checking net info for instance %s', port['id'])
+            info = self._get_sg_for_port(port['id'])
+            yield self._get_sample(port, info)
+
+class IncomingAcceptBytesPollster(_Base):
+    def _get_sample(self, port, info):
+        return self.make_sg_sample(
+            port,
+            name='sg.incoming.accept.bytes',
+            type=sample.TYPE_GAUGE,
+            unit='B',
+            volume=info.in_accept_bytes,
+        )
+
+class IncomingAcceptPktsPollster(_Base):
+    def _get_sample(self, port, info):
+        return self.make_sg_sample(
+            port,
+            name='sg.incoming.accept.pkts',
+            type=sample.TYPE_GAUGE,
+            unit='pkt',
+            volume=info.in_accept_packets,
+        )
+
+class IncomingDropBytesPollster(_Base):
+    def _get_sample(self, port, info):
+        return self.make_sg_sample(
+            port,
+            name='sg.incoming.drop.bytes',
+            type=sample.TYPE_GAUGE,
+            unit='B',
+            volume=info.in_drop_bytes,
+        )
+
+class IncomingDropPktsPollster(_Base):
+    def _get_sample(self, port, info):
+        return self.make_sg_sample(
+            port,
+            name='sg.incoming.drop.pkts',
+            type=sample.TYPE_GAUGE,
+            unit='pkt',
+            volume=info.in_drop_packets,
+        )
+
+class OutgoingAcceptBytesPollster(_Base):
+    def _get_sample(self, port, info):
+        return self.make_sg_sample(
+            port,
+            name='sg.outgoing.accept.bytes',
+            type=sample.TYPE_GAUGE,
+            unit='B',
+            volume=info.out_accept_bytes,
+        )
+
+class OutgoingAcceptPktsPollster(_Base):
+    def _get_sample(self, port, info):
+        return self.make_sg_sample(
+            port,
+            name='sg.outgoing.accept.pkts',
+            type=sample.TYPE_GAUGE,
+            unit='pkt',
+            volume=info.out_accept_packets,
+        )
+
+class OutgoingDropBytesPollster(_Base):
+    def _get_sample(self, port, info):
+        return self.make_sg_sample(
+            port,
+            name='sg.outgoing.drop.bytes',
+            type=sample.TYPE_GAUGE,
+            unit='B',
+            volume=info.out_drop_bytes,
+        )
+
+class IncomingDropPktsPollster(_Base):
+    def _get_sample(self, port, info):
+        return self.make_sg_sample(
+            port,
+            name='sg.outgoing.drop.pkts',
+            type=sample.TYPE_GAUGE,
+            unit='pkt',
+            volume=info.out_drop_packets,
+        )
---
+++ ceilometer/compute/sg_meter.py
@@ -0,0 +1,133 @@
+import subprocess
+from eventlet import greenthread
+
+INGRESS_DIRECTION = 'ingress'
+EGRESS_DIRECTION = 'egress'
+SG_CHAIN = 'sg-chain'
+SPOOF_FILTER = 'spoof-filter'
+CHAIN_PREFIX = 'neutron-openvswi-'
+CHAIN_NAME_PREFIX = {INGRESS_DIRECTION: 'i',
+                     EGRESS_DIRECTION: 'o',
+                     SPOOF_FILTER: 's'}
+MAX_CHAIN_LEN_NOWRAP = 28
+
+def execute(cmd):
+    try:
+        obj = subprocess.Popen(cmd,
+                                 stdin=subprocess.PIPE,
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE, shell=False)
+        _stdout, _stderr = obj.communicate()
+        returncode = obj.returncode
+        obj.stdin.close()
+    finally:
+        # NOTE(termie): this appears to be necessary to let the subprocess
+        #               call clean something up in between calls, without
+        #               it two execute calls in a row hangs the second one
+        greenthread.sleep(0)
+
+    return _stdout
+
+class SGmetering(object):
+    """Wrapper for iptables.
+
+    See IptablesTable for some usage docs
+
+    A number of chains are set up to begin with.
+
+    First, neutron-filter-top. It's added at the top of FORWARD and OUTPUT.
+    Its name is not wrapped, so it's shared between the various neutron
+    workers. It's intended for rules that need to live at the top of the
+    FORWARD and OUTPUT chains. It's in both the ipv4 and ipv6 set of tables.
+
+    For ipv4 and ipv6, the built-in INPUT, OUTPUT, and FORWARD filter chains
+    are wrapped, meaning that the "real" INPUT chain has a rule that jumps to
+    the wrapped INPUT chain, etc. Additionally, there's a wrapped chain named
+    "local" which is jumped to from neutron-filter-top.
+
+    For ipv4, the built-in PREROUTING, OUTPUT, and POSTROUTING nat chains are
+    wrapped in the same was as the built-in filter chains. Additionally,
+    there's a snat chain that is applied after the POSTROUTING chain.
+
+    """
+    def __init__(self, port):
+        print port
+        self.chain_in = self._get_port_chain_name(port, INGRESS_DIRECTION)
+        self.chain_out = self._get_port_chain_name(port, EGRESS_DIRECTION)
+        self.chain_spoof = self._get_port_chain_name(port, SPOOF_FILTER)
+        self.in_drop_bytes = 0
+        self.in_drop_packets = 0
+        self.in_accept_bytes = 0
+        self.in_accept_packets = 0
+        self.out_drop_bytes = 0
+        self.out_drop_packets = 0
+        self.out_accept_bytes = 0
+        self.out_accept_packets = 0
+        #self.get_traffic_counters(self.chain_in, zero=True)
+
+        acc_accept = {'pkts': 0, 'bytes': 0}
+        acc_drop = {'pkts': 0, 'bytes':0}
+
+        acc_accept['pkts'], acc_accept['bytes'], acc_drop['pkts'], acc_drop['bytes'] \
+                  = self._get_counters_by_chain(self.chain_in)
+        self.in_drop_bytes += acc_drop['bytes']
+        self.in_drop_packets += acc_drop['pkts']
+        self.in_accept_bytes += acc_accept['bytes']
+        self.in_accept_packets += acc_accept['pkts']
+
+        acc_accept['pkts'], acc_accept['bytes'], acc_drop['pkts'], acc_drop['bytes'] \
+                  = self._get_counters_by_chain(self.chain_spoof)
+        self.in_drop_bytes += acc_drop['bytes']
+        self.in_drop_packets += acc_drop['pkts']
+        self.in_accept_bytes += acc_accept['bytes']
+        self.in_accept_packets += acc_accept['pkts']
+
+        acc_accept['pkts'], acc_accept['bytes'], acc_drop['pkts'], acc_drop['bytes'] \
+                  = self._get_counters_by_chain(self.chain_out)
+        self.out_drop_bytes = acc_drop['bytes']
+        self.out_drop_packets = acc_drop['pkts']
+        self.out_accept_bytes = acc_accept['bytes']
+        self.out_accept_packets = acc_accept['pkts']
+
+    def _get_port_chain_name(self, port, direction):
+        return ('%s%s%s' % (CHAIN_PREFIX, CHAIN_NAME_PREFIX[direction], port))[:MAX_CHAIN_LEN_NOWRAP]
+
+    #    I think here have some performance issue.
+    #    When we have much china, it will exec iptable**** much time, I think it will be too bad.
+    #    Need get a good idea for it.
+    def _get_counters_by_chain(self, chain, zero=False):
+        """Return the sum of the traffic counters of all rules of a chain."""
+        acc_accept = {'pkts': 0, 'bytes': 0}
+        acc_drop = {'pkts': 0, 'bytes':0}
+
+        cmd = "iptables"
+        args = ['sudo', cmd,  '-L', chain, '-n', '-v', '-x']
+        if zero:
+            args.append('-Z')
+
+        current_table = execute(args)
+        current_lines = current_table.split('\n')
+
+        for line in current_lines[2:]:
+            if not line:
+                break
+            data = line.split()
+            if (len(data) < 2 or
+                    not data[0].isdigit() or
+                    not data[1].isdigit()):
+                break
+            if data[2] == 'ACCEPT' or data[2] == 'RETURN':
+                acc_accept['pkts'] += int(data[0])
+                acc_accept['bytes'] += int(data[1])
+            elif data[2] == 'DROP' or data[2] == 'neutron-openvswi-sg-fallback':
+                acc_drop['pkts'] += int(data[0])
+                acc_drop['bytes'] += int(data[1])
+            else:
+                pass
+
+        return acc_accept['pkts'], acc_accept['bytes'], acc_drop['pkts'], acc_drop['bytes']
+
+
+
+
+
